be041c5e201c0e482f30d9dbca712e86dc58be65
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-11-24 09:35:11 +0000

    feat: add issues field

--- error.go
+++ error.go
@@ -5,12 +5,15 @@ import (
 	"net/http"
 )
 
-func WithError(w http.ResponseWriter, msg string, status int) {
+func WithError(w http.ResponseWriter, msg string, status int, issues ...string) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
 	v := map[string]any{
 		"message":    msg,
 		"statusCode": int64(status),
+	}
+	if len(issues) > 0 {
+		v["issues"] = issues
 	}
 	json.NewEncoder(w).Encode(v)
 }
