8cf2ffaa1290b1487d4c72106d873ec265c43598
Joe M <joseph.mcgrath@aviva.com>
2024-04-29 09:46:57 +0100

    fix example

--- error.go
+++ error.go
@@ -8,11 +8,11 @@ import (
 // Error is a standard error response.
 type Error struct {
 	// Message is the error message.
-	Message string `json:"message",example:"The request was invalid."`
+	Message string `json:"message" example:"The request was invalid."`
 	// StatusCode is the HTTP status code.
-	StatusCode int `json:"statusCode",example:"400"`
+	StatusCode int `json:"statusCode" example:"400"`
 	// Issues is a list of issues with the request. This is optional.
-	Issues []string `json:"issues,omitempty",example:"The request was invalid."`
+	Issues []string `json:"issues,omitempty" example:"The request was invalid."`
 }
 
 func WithError(w http.ResponseWriter, msg string, status int, issues ...string) {
